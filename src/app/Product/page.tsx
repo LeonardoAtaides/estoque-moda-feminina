1bfe4b4e313092d5bc9569162b6b6e9eee86f2c4
Ataídes <162513948+LeonardoAtaides@users.noreply.github.com>
2026-04-10 23:57:35 -0300

    create page product

---
+++ src/app/Product/page.tsx
@@ -0,0 +1,19 @@
+"use client"
+import { ProductsTable } from "@/components/productstable";
+
+export default function ProductsPage() {
+ 
+  return (
+ 
+
+    <div className="">
+        <ProductsTable /> 
+        <div className="w-20">
+          <div className="bg-red-500 w-20 h-40 rounded-full"></div>
+        </div>
+        
+    </div>
+       
+
+  );
+}
