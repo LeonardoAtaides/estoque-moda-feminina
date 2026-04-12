f15e3f6a23a1121d6a079249ca2fb8759757489d
Ataídes <162513948+LeonardoAtaides@users.noreply.github.com>
2026-04-12 01:33:55 -0300

    Update eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -8,7 +8,8 @@ const eslintConfig = defineConfig([
 
   {
     rules: {
-      "react-hooks/set-state-in-effect": "off"
+      "react-hooks/set-state-in-effect": "off",
+      '@typescript-eslint/no-require-imports': 'off'
     }
   },
 
