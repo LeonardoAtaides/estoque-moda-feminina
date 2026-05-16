90343dbaeec1b6db1438046ce399bf77b325157c
ATAÍDES <162513948+LeonardoAtaides@users.noreply.github.com>
2026-05-16 01:05:58 -0300

    Update trends.int.test.ts

--- tests/trends.int.test.ts
+++ tests/trends.int.test.ts
@@ -1,10 +1,12 @@
+import { GET } from "@/app/api/trends/route";
+
 describe("GET /api/trends", () => {
   it("deve retornar 4 produtos válidos", async () => {
-    const res = await fetch("http://localhost:3000/api/trends");
+    const response = await GET();
 
-    const data = await res.json();
+    const data = await response.json();
 
-    expect(res.status).toBe(200);
+    expect(response.status).toBe(200);
 
     expect(Array.isArray(data)).toBe(true);
 
