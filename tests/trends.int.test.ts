036b8ba71f7dc2d5902373dfe134283504727765
ATAÍDES <162513948+LeonardoAtaides@users.noreply.github.com>
2026-05-16 01:03:47 -0300

    ajustandoi testes

--- tests/trends.int.test.ts
+++ tests/trends.int.test.ts
@@ -1,11 +1,19 @@
 describe("GET /api/trends", () => {
-  it("deve retornar 4 produtos", async () => {
+  it("deve retornar 4 produtos válidos", async () => {
     const res = await fetch("http://localhost:3000/api/trends");
 
     const data = await res.json();
 
     expect(res.status).toBe(200);
+
     expect(Array.isArray(data)).toBe(true);
+
     expect(data.length).toBe(4);
+
+    expect(data[0]).toHaveProperty("id");
+    expect(data[0]).toHaveProperty("title");
+    expect(data[0]).toHaveProperty("price");
+    expect(data[0]).toHaveProperty("rating");
+    expect(data[0]).toHaveProperty("thumbnail");
   });
 });
